3acc035bc24508b60c40ae16758c6dbf26c1b676
Sukhpreet singh <sukhpreet9808@gmail.com>
2025-04-08 23:56:54 +0530

    mm

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/', (req, res)=> {  // route on which we did get request and got respon
   res.send("hello im listening")
 })
 
-app.get('youtube', (req , res)=>{
+app.get('/youtube', (req , res)=>{
     res.send("this is my youtube channel")
 })
  app.listen(process.env.PORT, ()=> {  // listening at port
