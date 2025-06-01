879f2787a1c560dcf60418b1d8f79dc4f2487cb2
Sukhpreet singh <sukhpreet9808@gmail.com>
2025-06-01 21:47:46 +0530

    connecting to mongoDB with mongoose

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
 // require('dotenv').config()
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./src/db/index.js";
-import express from "express"
-
-
 
 dotenv.config();
 
 
 
-const app = express()
+connectDB();
 
-
+// const app = express()
 
 //   //  ******** First approach **********
 // using IIFE(Immediately Invoked Function Expression)
 
+// app.get('/', (req, res)=> {  // route on which we did get request and got response
+//   res.send("hello im listening")
+// })
 
-app.get('/', (req, res)=> {  // route on which we did get request and got response 
-  res.send("hello im listening")
-})
-
-app.get('/youtube', (req , res)=>{
-    res.send("this is my youtube channel")
-})
- app.listen(process.env.PORT, ()=> {  // listening at port
-    console.log(`app is listening on port ${process.env.PORT}`);
- })
-
-
-
+// app.get('/youtube', (req , res)=>{
+//     res.send("this is my youtube channel")
+// })
+//  app.listen(process.env.PORT, ()=> {  // listening at port
+//     console.log(`app is listening on port ${process.env.PORT}`);
+//  })
--- src/db/index.js
+++ src/db/index.js
@@ -7,11 +7,13 @@ const connectDB = async ()=> {
         //  Note:- is used to access the host information of 
         //  MongoDB server to which the application has successfully 
         // connected. 
-
+      
        const connectingDB = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
           console.log(`\n MongoDB connected !! DB HOST: ${connectingDB.connection.host}`);
+        //   console.log(`Connecting to: ${process.env.MONGODB_URL}/${DB_NAME}`);
+         console.log("Connected Successfully !!");
     } catch (error) {
-        console.log("MongoDB connection error", error);
+        console.log("MongoDB connection Failed", error);
         // current appn is running on some process so written below it is its reference
         process.exit(1)
     }
