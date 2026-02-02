9337ca36c8ae9b9ee22c8e7bb252fbf0d0f0b492
121496071+sukhii8954@user.noreply.github.com <12345678+sukhii8954@users.noreply.github.com>
2026-02-02 05:39:36 +0530

    feat: enhance like functionality for comments and tweets; update response statuses and error handling

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,8 +1,9 @@
-import mongoose, { isValidObjectId } from "mongoose"
+import mongoose, { isValidObjectId, mongo } from "mongoose"
 import { Like } from "../models/like.model.js"
 import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
+import { User } from "../models/user.model.js"
 
 
 
@@ -50,10 +51,10 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
         })
 
         return res.
-            status(200).
+            status(201).
             json(   // "isLiked" is a derived value sent to help the frontend update UI state  of like thumb to be filled or make it empty.
 
-                new ApiResponse(200, { isLiked: true }, "video liked Successfully...")
+                new ApiResponse(201, { isLiked: true }, "video liked Successfully...")
             )
     }
 
@@ -93,12 +94,12 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
     //TODO: toggle like on comment
 
     if (!isValidObjectId(commentId)) {
-        return new ApiError(400, "invalid comment ID")
+        throw new ApiError(400, "invalid comment ID")
     }
 
     const existingCommentLike = await Like.findOne({
         comment: commentId,
-        likedBy: req.user._id
+        likedBy: req.user?._id
     })
 
     if (existingCommentLike) {
@@ -116,13 +117,13 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
 
         await Like.create({
             comment: commentId,
-            likedBy: req.user._id
+            likedBy: req.user?._id
         })
 
         return res.
-            status(200).
+            status(201).
             json(
-                new ApiResponse(200, { isLiked: true }, "comment liked Successfully...")
+                new ApiResponse(201, { isLiked: true }, "comment liked Successfully...")
             )
 
     }
@@ -130,18 +131,238 @@ const toggleCommentLike = asyncHandler(async (req, res) => {
 
 })
 
+
+
+// ************************************************************
+
+
+
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const { tweetId } = req.params
     //TODO: toggle like on tweet
 
-    
-}
-)
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid Tweet Id");
+    }
+
+    const existingTweetLike = await Like.findOne({
+        tweet: tweetId,
+        likedBy: req.user?._id
+    })
+
+    if (existingTweetLike) {
+        await Like.findByIdAndDelete(existingTweetLike._id)
+
+        return res.
+            status(200).
+            json(
+                new ApiResponse(200, { isLiked: false }, "tweet unliked Successfully...")
+            )
+    }
+    else {
+        await Like.create({
+            tweet: tweetId,
+            likedBy: req.user?._id
+
+        })
+
+        return res.
+            status(201).
+            json(
+                new ApiResponse(201, { isLiked: true }, "tweet liked Successfully...")
+            )
+    }
+})
+
+
+// ************************************************************
+
+
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
     // here we use mongoose and its pipeline concept to match the ._id and lookup the liked videos and count it
+    // first I need to find all Like documents where likedBy = req.user._id
+    //  Then have to fetch the actual Video documents for those likes
+    /* 
+      Start from Like, not User
+      $match → likedBy user
+      $lookup → videos
+      $unwind / $first
+      (Optional) $lookup → owner
+      $project → clean response
+    */
 
+    // as video and likes are related to like collection so we start from there
+
+    // Start from Like, not User
+    const likedVideos = await Like.aggregate([
+
+        {
+            // $match → likedBy  logged in user
+            $match: {
+                likedBy: new mongoose.Types.ObjectId(String(req.user?._id))
+            }
+        },
+
+        // $lookup → videos (joins)  -> objectId will get replaced by video document here
+        {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "video",
+                // looking for video owner so using sub pipeline
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+
+                            pipeline: [
+                                {
+                                    $project: {
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+
+
+                      // Note:- 
+        /* 
+             we used addfields above for making easier for frontend mainly
+            for ex:- 
+            before flattening the owner and video array our output may be look like this :-
+            
+
+            after first lookup:-  oining videos document with like document
+                   
+                {
+                 _id: "likeId",
+                      // video is an array not object :- point to be noted
+                     video: [
+                        {
+                          _id: "videoId",
+                          title: "Mongo Aggregation",
+                           owner:; objectId("userId")
+                        }
+                      ]
+                    }
+ 
+             in video lookup we wrote sub pipeline  then output will looks like this 
+             of Video document :-
+              
+              owner is also an array here : because lookup return array 
+              
+             {
+                _id: "videoId",
+                title: "Mongo Aggregation",
+               owner: [
+               {
+               _id: "userId",
+               username: "hitesh",
+               avatar: "avatar.png"
+                }
+              ]
+            }
+
+            
+        */
+
+        //   / flattening owner array 
+        // means From the owner array, take the first element and replace the array with that object
+                    {
+                        $addFields: {
+                            owner: { $first: "$owner" }
+                        }
+                    }
+                ]
+            }
+        },
+
+        /* after flattening above :- 
+          From the owner array, take the first element and replace the array with that object
+          So that becomes:-
+
+           owner: {
+              uusername: "xyz",
+              avatar: "avatar.png,
+           }
+            
+           ####### so in frontend we can directly write owner.username instead of owner[0].username
+
+           similar we did for video
+
+
+        */
+        
+
+        //   flattening video array
+        // means : convert the single element video array into single object
+
+        {
+            $addFields: {
+                video: { $first: "$video" }
+            }
+        },
+
+        /*
+         after the outer lookup our video structure looks like this:-
+           {
+             video:: [
+              {
+               title: "Mongo Aggregation",
+               owner: {username: "xyz"} 
+              } 
+             ]
+           }
+
+           ---->>> but frontend expects an object not array 
+           like this :- 
+           
+           video: {
+               title: "Mongo Aggregation",
+               owner: {username: "xyz"} 
+            } 
+
+           so we flatten the video array --> which convert the single element video array into single object
+         
+        
+        */ 
+
+        // for frontend display we can use this :- 
+
+        // {
+        //     $project: {
+        //         _id: 0,
+        //         likedAt: "$createdAt",
+        //         video: {
+        //             _id: 1,
+        //             title: 1,
+        //             thumbnail: 1,
+        //             views: 1,
+        //             owner: 1
+        //         }
+        //     }
+        // }
+
+
+    ])
+
+
+    return res.
+        status(200).
+        json(new ApiResponse(
+            200,
+            likedVideos,
+            "Liked videos fetched successfully"
+        )
+        )
 
 })
 
